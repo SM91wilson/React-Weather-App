27b7bd5dbf0177bdb55dbdceed8af5e511bdce9a
tango <sm91wilson@gmail.com>
2021-04-01 00:31:17 -0400

    added searchbar

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,11 +2,28 @@
 // import './App.css';
 import React from "react";
 import Header from "./components/header/header";
+import SearchBar from "./components/searchBar/searchBar";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div>
+        <Header />
+      </div>
+      <div class ='container'>
+        <div class ='row'>
+          <div><SearchBar /></div>
+        </div>
+        <div>{/* <recent searches> */}</div>
+      </div>
+      <div class='container'>
+        <div class='row'>
+          <div>
+            {/* <weather> */}
+            {/* <forecast> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
