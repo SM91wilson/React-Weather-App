9ec91e8af85ab1558b2493003048911c0717e151
tango <sm91wilson@gmail.com>
2021-04-01 00:50:12 -0400

    added button for search bar

--- weather-app/src/components/searchBar/searchBar.js
+++ weather-app/src/components/searchBar/searchBar.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function SearchBar () {
     return(
+        <form>
         <input type='text' placeholder="Search"></input>
+        <button type='submit'>Submit</button>
+        </form>
     )
 }
 
